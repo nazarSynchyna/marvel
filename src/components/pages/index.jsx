3708f6fe7c190ea2966c39ba0000a58416c5dfac
Nazar Synchyna <nsynchyna@gmail.com>
2026-08-31 15:54:09 +0300

    feat(pages): create 404 error page component

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -1,4 +1,5 @@
 import MainPage from "./mainPage";
 import ComicsPage from "./ComicsPage";
+import Page404 from "./404";
 
-export { MainPage, ComicsPage };
+export { MainPage, ComicsPage, Page404 };
