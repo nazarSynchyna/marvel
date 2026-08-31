d84002c876b832d4c85a06c88177a2a65a8601d5
Nazar Synchyna <nsynchyna@gmail.com>
2026-08-31 16:45:49 +0300

    refactor(pages): move singleComicPage components and styles to pages directory

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -1,5 +1,6 @@
 import MainPage from "./mainPage";
 import ComicsPage from "./ComicsPage";
 import Page404 from "./404";
+import SingleComicPage from "./SingleComicPage";
 
-export { MainPage, ComicsPage, Page404 };
+export { MainPage, ComicsPage, Page404, SingleComicPage };
