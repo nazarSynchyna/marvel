1379433e917427c20be732a223b07257735e99c7
Nazar Synchyna <nsynchyna@gmail.com>
2026-08-31 17:06:31 +0300

    feat(404): add button to navigate to previous page via useNavigate

--- src/components/pages/404.jsx
+++ src/components/pages/404.jsx
@@ -1,13 +1,32 @@
 import ErrorMessage from "../errorMessage/errorMessage";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Page404 = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <ErrorMessage />
       <p style={{ textAlign: "center", fontWeight: "bold", fontSize: "24px" }}>
         Page doesn't exist
       </p>
+      <button
+        style={{
+          display: "block",
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: "24px",
+          marginTop: "30px",
+          marginInline: "auto",
+          background: "none",
+          border: "none",
+          cursor: "pointer",
+          color: "#9f0013",
+        }}
+        onClick={() => navigate(-1)}
+      >
+        Go back to previous page
+      </button>
       <Link
         style={{
           display: "block",
