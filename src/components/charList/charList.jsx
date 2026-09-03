dca77e8f287d715f4facd7c99eb6c68333bae6d6
Nazar Synchyna <nsynchyna@gmail.com>
2026-09-03 20:06:55 +0300

    feat(charList): add transition group animations and item focus handling

--- src/components/charList/charList.jsx
+++ src/components/charList/charList.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, createRef } from "react";
 import PropTypes from "prop-types";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/errorMessage";
@@ -25,25 +26,28 @@ const CharList = (props) => {
 
   const onCharListLoaded = (newCharList) => {
     let ended = false;
-
     if (newCharList.length < 9) {
       ended = true;
     }
 
     setCharList((charList) => [...charList, ...newCharList]);
-    setNewItemLoading((newItemLoading) => false);
+    setNewItemLoading(false);
     setOffset((offset) => offset + 9);
-    setCharEnded((charEnded) => ended);
+    setCharEnded(ended);
   };
 
   const itemRefs = useRef([]);
 
   const focusOnItem = (id) => {
-    itemRefs.current.forEach((item) =>
-      item.classList.remove("char__item_selected"),
-    );
-    itemRefs.current[id].classList.add("char__item_selected");
-    itemRefs.current[id].focus();
+    itemRefs.current.forEach((item) => {
+      if (item && item.current) {
+        item.current.classList.remove("char__item_selected");
+      }
+    });
+    if (itemRefs.current[id] && itemRefs.current[id].current) {
+      itemRefs.current[id].current.classList.add("char__item_selected");
+      itemRefs.current[id].current.focus();
+    }
   };
 
   function renderItems(arr) {
@@ -53,44 +57,58 @@ const CharList = (props) => {
         imgStyle = { objectFit: "unset" };
       }
 
+      if (!itemRefs.current[i]) {
+        itemRefs.current[i] = createRef();
+      }
+
       return (
-        <li
-          tabIndex={0}
-          ref={(el) => (itemRefs.current[i] = el)}
-          className="char__item"
+        <CSSTransition
           key={item.id ? item.id : i}
-          onClick={() => {
-            props.onCharSelected(item.id);
-            focusOnItem(i);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === " " || e.key === "Enter") {
+          timeout={500}
+          classNames="char__item"
+          appear={true}
+          nodeRef={itemRefs.current[i]}
+        >
+          <li
+            tabIndex={0}
+            ref={itemRefs.current[i]}
+            className="char__item"
+            onClick={() => {
               props.onCharSelected(item.id);
               focusOnItem(i);
-            }
-          }}
-        >
-          <img
-            src={item.thumbnail}
-            alt={item.name}
-            style={imgStyle}
-            onError={(e) => {
-              e.currentTarget.onerror = null;
-              e.target.src =
-                "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg";
-              e.target.style.objectFit = "unset";
             }}
-          />
-          <div className="char__name">{item.name}</div>
-        </li>
+            onKeyDown={(e) => {
+              if (e.key === " " || e.key === "Enter") {
+                props.onCharSelected(item.id);
+                focusOnItem(i);
+              }
+            }}
+          >
+            <img
+              src={item.thumbnail}
+              alt={item.name}
+              style={imgStyle}
+              onError={(e) => {
+                e.currentTarget.onerror = null;
+                e.target.src =
+                  "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg";
+                e.target.style.objectFit = "unset";
+              }}
+            />
+            <div className="char__name">{item.name}</div>
+          </li>
+        </CSSTransition>
       );
     });
 
-    return <ul className="char__grid">{items}</ul>;
+    return (
+      <ul className="char__grid">
+        <TransitionGroup component={null}>{items}</TransitionGroup>
+      </ul>
+    );
   }
 
   const items = renderItems(charList);
-
   const errorMessage = error ? <ErrorMessage /> : null;
   const spinner = loading && !newItemLoading ? <Spinner /> : null;
 
